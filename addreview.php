dc892d445fb868f13fe3dd66095929f15a7258b4
verma-kartik <kartikv.vero@gmail.com>
2021-05-20 15:31:16 +0530

    added form and php file

---
+++ addreview.php
@@ -0,0 +1,30 @@
+<?php
+
+$hostname = "localhost";
+$username = "root";
+$password = "302076";
+$db = "reviews";
+
+$dbconnect=mysqli_connect($hostname,$username,$password,$db);
+
+if ($dbconnect->connect_error) {
+  die("Database connection failed: " . $dbconnect->connect_error);
+}
+
+if(isset($_POST['submit'])) {
+  $reviewer_name=$_POST['reviewer_name'];
+  $star_rating=$_POST['star_rating'];
+  $details=$_POST['details'];
+
+  $query = "INSERT INTO user_review (reviewer_name, star_rating, details)
+  VALUES ('$reviewer_name', '$star_rating', '$details')";
+
+    if (!mysqli_query($dbconnect, $query)) {
+        die('An error occurred. Your review has not been submitted.');
+    } else {
+      echo "Thanks for your review.";
+    }
+
+}
+?>
+
